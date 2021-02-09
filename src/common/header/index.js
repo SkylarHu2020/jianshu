db756c1352c24b59c140045dcf9864c5ed3441f8
SkylarHu <skylar@heyavo.com>
2021-02-09 14:34:57 +0800

    change to hook header index file

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,19 +1,63 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { HeaderWrapper, Logo, Nav, NavItem,
   NavSearch, Addition, Button, SearchWrapper,
   SearchInfo, SearchInfoTitle, SearchInfoSwitch,
   SearchInfoItem, SearchInfoList } from './style'
 import { CSSTransition } from 'react-transition-group'
-import { connect } from 'react-redux'
 import { actionCreators } from './store'
 import { actionCreators as loginActionCreators } from '../../pages/login/store'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector} from 'react-redux'
 
-class Header extends PureComponent {
+const Header = () => {
+  const focused = useSelector((state) => state.getIn(['header', 'focused']))
+  const list = useSelector((state) => state.getIn(['header', 'list']))
+  const page = useSelector((state) => state.getIn(['header', 'page']))
+  const totalPage = useSelector((state) => state.getIn(['header', 'totalPage']))
+  const mouseIn = useSelector((state) => state.getIn(['header', 'mouseIn']))
+  const login = useSelector((state) => state.getIn(['login', 'login']))
+  const dispatch = useDispatch()
 
-  getListArea () {
-    const { focused, list, page, totalPage, mouseIn,
-      handleMouseEnter, handleMouseLeave, handleChangePage } = this.props
+  const handleInputFocused = () => {
+    if (list.size === 0) {
+      dispatch(actionCreators.getList())
+    }
+    dispatch(actionCreators.searchFocus())
+  }
+
+  const handleInputBlur = () => {
+    dispatch(actionCreators.searchBlur())
+  }
+
+  const handleMouseEnter = () => {
+    dispatch(actionCreators.mouseEnter())
+  }
+
+  const handleMouseLeave = () => {
+    dispatch(actionCreators.mouseLeave())
+  }
+
+  const handleChangePage = (page, totalPage, spin) => {
+    let orginAngel = spin.style.transform.replace(/[^0-9]/ig, '')
+    if (orginAngel) {
+      orginAngel = parseInt(orginAngel)
+    } else {
+      orginAngel = 0
+    }
+
+    spin.style.transform = 'rotate(' + (orginAngel + 360) + 'deg)'
+    if (page < totalPage) {
+      dispatch(actionCreators.changePage(page + 1))
+    } else {
+      dispatch(actionCreators.changePage(1))
+    }
+  }
+
+  const logout = () => {
+    dispatch(loginActionCreators.logout())
+  }
+
+  const getListArea = () => {
     const jsList = list.toJS()
     const pageList = []
     const indexEnd = Math.min(page * 10, jsList.length - 1)
@@ -45,104 +89,50 @@ class Header extends PureComponent {
     }
   }
 
-  render () {
-    const { focused, list, login, handleInputBlur, handleInputFocused, logout } = this.props
-    return (
-      <HeaderWrapper>
-        <Link to='/'>
-          <Logo/>
+  return (
+    <HeaderWrapper>
+      <Link to='/'>
+        <Logo/>
+      </Link>
+      <Nav>
+        <NavItem className='left active'>首页</NavItem>
+        <NavItem className='left'>下载App</NavItem>
+        {
+          login ?
+            <NavItem className='right' onClick={logout}>退出</NavItem> :
+            <NavItem className='right'><Link to='/login'>登录</Link></NavItem>
+        }
+        <NavItem className='right'>
+          <i className="iconfont">&#xe636;</i>
+        </NavItem>
+        <SearchWrapper>
+          <CSSTransition
+            timeout={200}
+            in={focused}
+            classNames="slide"
+          >
+            <NavSearch
+              className={focused ? 'focused' : ''}
+              onFocus={() => handleInputFocused(list)}
+              onBlur={handleInputBlur}
+            ></NavSearch>
+          </CSSTransition>
+          <i className={focused ? 'focused iconfont zoom' : 'iconfont zoom'}>&#xe62d;</i>
+          {getListArea()}
+        </SearchWrapper>
+      </Nav>
+      <Addition>
+        <Link to='/write'>
+          <Button className='writting'>
+            <i className="iconfont">&#xe6e5;</i>
+            写文章
+          </Button>
         </Link>
-        <Nav>
-          <NavItem className='left active'>首页</NavItem>
-          <NavItem className='left'>下载App</NavItem>
-          {
-            login ?
-              <NavItem className='right' onClick={logout}>退出</NavItem> :
-              <NavItem className='right'><Link to='/login'>登录</Link></NavItem>
-          }
-          <NavItem className='right'>
-            <i className="iconfont">&#xe636;</i>
-          </NavItem>
-          <SearchWrapper>
-            <CSSTransition
-              timeout={200}
-              in={focused}
-              classNames="slide"
-            >
-              <NavSearch
-                className={focused ? 'focused' : ''}
-                onFocus={() => handleInputFocused(list)}
-                onBlur={handleInputBlur}
-              ></NavSearch>
-            </CSSTransition>
-            <i className={focused ? 'focused iconfont zoom' : 'iconfont zoom'}>&#xe62d;</i>
-            {this.getListArea()}
-          </SearchWrapper>
-        </Nav>
-        <Addition>
-          <Link to='/write'>
-            <Button className='writting'>
-              <i className="iconfont">&#xe6e5;</i>
-              写文章
-            </Button>
-          </Link>
-          <Button className='reg'>注册</Button>
-        </Addition>
-      </HeaderWrapper>
-    )
-  }
+        <Button className='reg'>注册</Button>
+      </Addition>
+    </HeaderWrapper>
+  )
 }
 
-// state: store 中的 state
-const mapStateToProps = (state) => {
-  return {
-    // immutable 对象不能用.来调用数据，使用get('name')
-    // === state.getIn(['header', 'focused'])
-    focused: state.get('header').get('focused'),
-    list: state.get('header').get('list'),
-    page: state.get('header').get('page'),
-    totalPage: state.get('header').get('totalPage'),
-    mouseIn: state.get('header').get('mouseIn'),
-    login: state.get('login').get('login')
-  }
-}
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    handleInputFocused (list) {
-      if (list.size === 0) {
-        dispatch(actionCreators.getList())
-      }
-      dispatch(actionCreators.searchFocus())
-    },
-    handleInputBlur () {
-      dispatch(actionCreators.searchBlur())
-    },
-    handleMouseEnter () {
-      dispatch(actionCreators.mouseEnter())
-    },
-    handleMouseLeave () {
-      dispatch(actionCreators.mouseLeave())
-    },
-    handleChangePage (page, totalPage, spin) {
-      let orginAngel = spin.style.transform.replace(/[^0-9]/ig, '')
-      if (orginAngel) {
-        orginAngel = parseInt(orginAngel)
-      } else {
-        orginAngel = 0
-      }
-
-      spin.style.transform = 'rotate(' + (orginAngel + 360) + 'deg)'
-      if (page < totalPage) {
-        dispatch(actionCreators.changePage(page + 1))
-      } else {
-        dispatch(actionCreators.changePage(1))
-      }
-    },
-    logout () {
-      dispatch(loginActionCreators.logout())
-    }
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Header)
+export default Header
